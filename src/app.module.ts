2b59e51a0014ca94bf2ec7f21fafccf739ac416f
hana <70460699+IMHANA@users.noreply.github.com>
2021-07-05 01:22:22 +0900

    [update] 모듈 분리 (app module / movies module]

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
-
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './movies/app.controller';
+// app.module은 AppController와 AppProvider만 가지고 있어야 한다.
+// 그래서 여기있는 MoviesService랑 MoviesController를 movies.module로 옮김
+// 왜냐면 NestJS에서 앱은 여러개의 모듈로 구성되어 있음
 @Module({
-  imports: [],
-  controllers: [MoviesController],
-  providers: [MoviesService],
+  imports: [MoviesModule],
+  controllers: [AppController],
+  providers: [],
   exports: [],
 })
 export class AppModule {}
